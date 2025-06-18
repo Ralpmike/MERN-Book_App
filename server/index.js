a5c4018e3ed5d97dbaab81b30b41cded41e7858c
Ralpmike <ralphmike996@gmail.com>
2025-06-18 08:26:55 +0100

    static file url

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ setUpMiddleware(app);
 // app.use(cors());
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
-app.use("/uploads", express.static("public/uploads"));
+// app.use("/uploads", express.static("public/uploads"));
 
 app.use("/api/books", BooksRouter);
 
